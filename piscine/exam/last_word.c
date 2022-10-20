b86a383519daa0c5a23ef90c99a6d144f97337ba
Jeroen Van Halderen <jvan-hal@f0r4s7.codam.nl>
2022-10-20 13:23:28 +0200

    cleaned up functions that I made in libft in my piscine folder

--- piscine/exam/last_word.c
+++ piscine/exam/last_word.c
@@ -10,19 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include"libft.h"
 #include<stdio.h>
-
-int ft_strlen(char *str)
-{
-    int i;
-
-    i = 0;
-    while (str[i])
-    {
-        i++;
-    }
-    return i;
-}
 
 char    *last_word(char *str)
 {
