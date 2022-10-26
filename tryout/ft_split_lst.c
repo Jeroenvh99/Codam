a9057660128a21264abbfad59a8410a727bb5682
Jeroen Van Halderen <jvan-hal@f0r6s4.codam.nl>
2022-10-26 09:10:50 +0200

    made ft_split with a linked list

--- tryout/ft_split_lst.c
+++ tryout/ft_split_lst.c
@@ -1,3 +1,13 @@
+#include"libft.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+void	del(void *node)
+{
+	free(node);
+	node = NULL;
+}
+
 void printlist(t_list *head)
 {
 	while (head)
@@ -7,37 +17,90 @@ void printlist(t_list *head)
 	}
 }
 
-int ft_split_lst(char const *s, char c)
+static char	*getword(const char *s, int len)
 {
-	t_list	*splits;
+	char	*split;
 	int		i;
-	int		j;
+
+	i = 0;
+	split = malloc(len + 1);
+	if (!split)
+		return (NULL);
+	while (len)
+	{
+		split[i] = *s;
+		++s;
+		++i;
+		--len;
+	}
+	split[i] = '\0';
+	return (split);
+}
+
+static char	*getsplit(const char **s, char dlm)
+{
+	int		i;
 	char	prevc;
 	char	*split;
 
-	j = 0;
+	i = 0;
 	prevc = dlm;
-	while (j == 0 || c[j - 1])
+	while (i == 0 || s[0][i - 1])
 	{
-		if ((s[j] == c && prevc != c) || (!s[j] && prevc != c))
+		if ((s[0][i] == dlm && prevc != dlm) || (!(s[0][i])
+			&& prevc != dlm))
 		{
-			i = 0;
-			split = malloc(j);
-			while (i < j)
-			{
-				split[i] = *s;
-				++i;
-				++s;
-			}
-			c[i] = '\0';
-			j = 0;
-			ft_lstadd_back(ft_lstnew(c));
+			split = getword(s[0], i);
+			if (!split)
+				return (NULL);
+			s[0] += i;
+			return (split);
 		}
-		prevc = c[j];
-		if (s[j] == c)
-			++j;
-		else
-			j = 0;
+		prevc = s[0][i];
+		++i;
 	}
-	return (1);
+	return (NULL);
+}
+
+t_list	*ft_split_lst(char const *s, char c)
+{
+	t_list	*arr;
+	t_list	*newnode;
+
+	arr = NULL;
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		while (*s == c)
+			++s;
+		newnode = ft_lstnew(getsplit(&s, c));
+		if (arr && (!newnode || !newnode->content))
+		{
+			ft_lstclear(&arr, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&arr, newnode);
+	}
+	return (arr);
+}
+
+int main(){
+	t_list *splitarr;
+	splitarr = ft_split_lst("      split       this for   me  !       hfhjutyk", ' ');
+	printlist(splitarr);
+	ft_lstclear(&splitarr, del);
+	splitarr = NULL;
+	splitarr = ft_split_lst(NULL, ' ');
+	printlist(splitarr);
+	ft_lstclear(&splitarr, del);
+	splitarr = NULL;
+	splitarr = ft_split_lst("vdfbnjfgbnghvbdfkergh", 'b');
+	printlist(splitarr);
+	ft_lstclear(&splitarr, del);
+	splitarr = NULL;
+	splitarr = ft_split_lst("Yay,   it works! !!??", ' ');
+	printlist(splitarr);
+	ft_lstclear(&splitarr, del);
+	return 0;
 }
