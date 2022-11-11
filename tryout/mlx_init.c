50b77879e1b3ace073e210150380fd55ef4c300f
Jeroen Van Halderen <jvan-hal@f0r3s6.codam.nl>
2022-11-11 17:42:48 +0100

    made my improvement for MLX

--- tryout/mlx_init.c
+++ tryout/mlx_init.c
@@ -1,32 +1,40 @@
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture0"), 0);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture1"), 1);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture2"), 2);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture3"), 3);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture4"), 4);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture5"), 5);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture6"), 6);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture7"), 7);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture8"), 8);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture9"), 9);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture10"), 10);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture11"), 11);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture12"), 12);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture13"), 13);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture14"), 14);
-glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture15"), 15);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture0"), 0);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture1"), 1);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture2"), 2);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture3"), 3);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture4"), 4);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture5"), 5);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture6"), 6);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture7"), 7);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture8"), 8);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture9"), 9);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture10"), 10);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture11"), 11);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture12"), 12);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture13"), 13);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture14"), 14);
+// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, "Texture15"), 15);
+#include<stdio.h>
 
-char nbstr[10] = "Texture";
-for (int i = 0; i < 16; i++)
+int main()
 {
-	nbstr[7] = i + '0';
-	if (i > 9)
+	char nbstr[10] = "Texture";
+	int i = 7;
+	int jtmp;
+	for (int j = 0; j < 16; j++)
 	{
-		nbstr[8] = i + '0';
-		nbstr[9] = '\0';
+		jtmp = j;
+		while (jtmp > 9)
+		{
+			nbstr[i] = (jtmp / 10) + '0';
+			jtmp %= 10;
+			++i;
+		}
+		nbstr[i] = jtmp + '0';
+		++i;
+		nbstr[i] = '\0';
+		printf("%i\n", printf("%s ", nbstr));
+		i = 7;
+		// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, nbstr), i);
 	}
-	else
-	{
-		nbstr[7] = '\0';
-	}
-	// glUniform1i(glGetUniformLocation(mlxctx->shaderprogram, nbstr), i);
 }
